9b3e51d685a9f9204663b87b1d2bef065ce74982
Lorhaki <flament.lolo@laposte.net>
2022-11-22 10:15:19 +0100

    initial commit

---
+++ src/test/java/fr/projet/bddweb/BddwebApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projet.bddweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BddwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
